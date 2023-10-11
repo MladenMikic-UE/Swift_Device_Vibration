88fe7e772d21fa535165e18e7a0ecf4f180d1514
Mladen.Mikic <mladen.mikic@versuni.com>
2023-10-11 16:34:56 +0200

    Initial Commit

---
+++ Swift_Device_Vibration/Swift_Device_VibrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Device_VibrationApp.swift
+//  Swift_Device_Vibration
+//
+//  Created by MladenMikic# on 11.10.2023..
+//
+
+import SwiftUI
+
+@main
+struct Swift_Device_VibrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
